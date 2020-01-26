28df60adb341db9395ef37be27ab3ca5e1c9b312
Will Vedder <willvedder@Sidecars-MacBook-Pro.local>
2020-01-26 09:37:26 -0500

    Updates

---
+++ precache-manifest.54c93bc2d2e32be8dd23e74a5ff56b0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6215986ea144f650e6aff5ef4a19b815",
+    "url": "/react-concurrent-mode/index.html"
+  },
+  {
+    "revision": "af99e71a3d9610f54427",
+    "url": "/react-concurrent-mode/static/js/2.e240e242.chunk.js"
+  },
+  {
+    "revision": "32aecf3e3e1e2b6fbd07",
+    "url": "/react-concurrent-mode/static/js/main.f822472f.chunk.js"
+  },
+  {
+    "revision": "ea03fc06dd4f385cdfdc",
+    "url": "/react-concurrent-mode/static/js/runtime-main.f78db7c5.js"
+  }
+]);
