d7539b87d6cb1eee4a3e246c6783c3978c8de91e
Will Vedder <willvedder@Sidecars-MacBook-Pro.local>
2020-01-26 11:04:51 -0500

    Added homepage config and deploy scripts for deploying to gh-pages, ignore build

--- demo/src/Autocomplete/mockApi.js
+++ demo/src/Autocomplete/mockApi.js
@@ -41,6 +41,15 @@ export function fetchWords(text) {
           .map(word => {
             return word.charAt(0).toUpperCase() + word.slice(1);
           })
+          .sort((a, b) => {
+            if (a.length > b.length) {
+              return -1;
+            } else if (a.length < b.length) {
+              return 1;
+            } else {
+              return 0;
+            }
+          })
       );
     }, 400)
   );
